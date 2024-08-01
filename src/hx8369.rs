77b814261df12bc3a469031b9be76f2947294c67
Chen Xu <windoze@0d0a.com>
2024-08-01 23:07:44 +0800

    Use generated bindings

--- src/hx8369.rs
+++ src/hx8369.rs
@@ -1,6 +1,6 @@
 use std::{
     cmp::{max, min},
-    ffi::c_int,
+    ffi::c_void,
 };
 
 use std::convert::Infallible;
@@ -10,57 +10,22 @@ use embedded_graphics::{
     prelude::{Dimensions, DrawTarget, Point, Size},
     primitives::Rectangle,
 };
+use esp_idf_svc::sys::{
+    esp_lcd_panel_draw_bitmap, esp_lcd_panel_handle_t, esp_lcd_panel_invert_color,
+    esp_lcd_panel_mirror, esp_lcd_panel_reset, esp_lcd_panel_set_gap, esp_lcd_panel_swap_xy,
+    hx8369_init,
+};
 
-#[allow(non_camel_case_types)]
-#[repr(C)]
-struct esp_lcd_panel_t {
-    _data: [u8; 0],
-    _marker: core::marker::PhantomData<(*mut u8, core::marker::PhantomPinned)>,
-}
-
-type EspLcdPanelHandleT = *mut esp_lcd_panel_t;
-
-#[allow(non_camel_case_types)]
-type esp_err_t = i32;
-
-#[allow(dead_code)]
-extern "C" {
-    fn hx8369_init() -> *mut esp_lcd_panel_t;
-
-    fn esp_lcd_panel_reset(panel: EspLcdPanelHandleT) -> esp_err_t;
-
-    fn esp_lcd_panel_init(panel: EspLcdPanelHandleT) -> esp_err_t;
-
-    fn esp_lcd_panel_draw_bitmap(
-        panel: *mut esp_lcd_panel_t,
-        x_start: i32,
-        y_start: i32,
-        x_end: i32,
-        y_end: i32,
-        color_data: *const u8,
-    ) -> esp_err_t;
-
-    fn esp_lcd_panel_mirror(panel: EspLcdPanelHandleT, mirror_x: bool, mirror_y: bool)
-        -> esp_err_t;
-
-    fn esp_lcd_panel_swap_xy(panel: EspLcdPanelHandleT, swap_axes: bool) -> esp_err_t;
-
-    fn esp_lcd_panel_set_gap(panel: EspLcdPanelHandleT, x_gap: c_int, y_gap: c_int) -> esp_err_t;
-
-    fn esp_lcd_panel_invert_color(panel: EspLcdPanelHandleT, invert_color_data: bool) -> esp_err_t;
-
-    fn esp_lcd_panel_disp_on_off(panel: EspLcdPanelHandleT, on_off: bool) -> esp_err_t;
-}
-
+#[derive(Debug)]
 pub struct HX8369 {
-    handle: *mut esp_lcd_panel_t,
+    handle: esp_lcd_panel_handle_t,
 
     width: usize,
     height: usize,
     buffer: Vec<u16>,
 
-    min_y: usize,
-    max_y: usize,
+    min_dirty_y: usize,
+    max_dirty_y: usize,
 }
 
 const LINES: usize = 60;
@@ -75,8 +40,8 @@ impl HX8369 {
             height,
             buffer: vec![0; width * height],
 
-            min_y: height,
-            max_y: 0,
+            min_dirty_y: height,
+            max_dirty_y: 0,
         }
     }
 
@@ -95,7 +60,7 @@ impl HX8369 {
                 y_start,
                 x_end,
                 y_end,
-                color_data as *const _ as *const u8,
+                color_data as *const _ as *const c_void,
             )
         }
     }
@@ -129,23 +94,23 @@ impl HX8369 {
     }
 
     pub fn invalidate(&mut self) {
-        self.min_y = 0;
-        self.max_y = self.height;
+        self.min_dirty_y = 0;
+        self.max_dirty_y = self.height;
     }
 
     pub fn flush(&mut self) {
         // Nothing to flush
-        if self.min_y > self.max_y {
+        if self.min_dirty_y > self.max_dirty_y {
             // Nothing to flush
             return;
         }
         // HX8369 can only send ~100K bytes at once, about 800x62 pixels in RGB565 format
         // so we need to split the buffer into chunks, LINES is set to 60 as we have screen height at 480
         // Flush in chunks of LINES
-        for i in (self.min_y..self.max_y).step_by(LINES) {
+        for i in (self.min_dirty_y..self.max_dirty_y).step_by(LINES) {
             unsafe {
                 // Don't exceed screen bounds, as well as max_y
-                let y_end = min(min(i + LINES, self.height), self.max_y + 1);
+                let y_end = min(min(i + LINES, self.height), self.max_dirty_y + 1);
                 // Swap start and end if needed
                 let y_start = min(i, y_end);
                 let y_end = max(i, y_end);
@@ -161,12 +126,12 @@ impl HX8369 {
                     self.width as i32,
                     y_end as i32,
                     (&self.buffer.as_slice()[y_start * self.width..y_end * self.width]) as *const _
-                        as *const u8,
+                        as *const c_void,
                 );
             }
         }
-        self.min_y = self.height;
-        self.max_y = 0;
+        self.min_dirty_y = self.height;
+        self.max_dirty_y = 0;
     }
 
     pub fn get_raw_buffer_mut<T>(&mut self) -> &mut [T] {
@@ -196,28 +161,12 @@ impl HX8369 {
     }
 
     pub fn fill(&mut self, color: Rgb565) {
-        self.min_y = 0;
-        self.max_y = self.height;
+        self.min_dirty_y = 0;
+        self.max_dirty_y = self.height;
         self.get_raw_buffer_mut()
             .iter_mut()
             .for_each(|p| *p = color);
         self.flush();
-    }
-}
-
-pub trait DrawCanvas {
-    type PixelColor: Clone
-        + Copy
-        + embedded_graphics::prelude::RgbColor
-        + From<embedded_graphics::pixelcolor::Rgb888>
-        + Into<embedded_graphics::pixelcolor::Rgb888>;
-    fn get_pixel(&self, x: usize, y: usize) -> Self::PixelColor;
-}
-
-impl DrawCanvas for HX8369 {
-    type PixelColor = Rgb565;
-    fn get_pixel(&self, x: usize, y: usize) -> Self::PixelColor {
-        self.get_pixel(x, y)
     }
 }
 
@@ -250,11 +199,11 @@ impl DrawTarget for HX8369 {
             if x >= self.width || y >= self.height {
                 continue;
             }
-            if self.min_y > y {
-                self.min_y = y;
+            if self.min_dirty_y > y {
+                self.min_dirty_y = y;
             }
-            if self.max_y < y {
-                self.max_y = y;
+            if self.max_dirty_y < y {
+                self.max_dirty_y = y;
             }
             if x < self.width && y < self.height {
                 let w = self.width;
